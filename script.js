a827040279b088d035ac4a8cfcd4b356dc3a029c
jfark <jessamyn.farkas@gmail.com>
2020-09-20 19:13:11 -0400

    Tried to change the Carousel angle script equation so it looks like the original

--- script.js
+++ script.js
@@ -25,13 +25,12 @@ Vue.component('the-portfolio', {
 	methods: {
 		rotateCarousel: function() {
 			var cellCount = this.project.rooms.length;
-			var cellRange = 8;
-			var theta = 360 / cellRange;
+			/*var cellRange = 8;*/
+			var theta = 360 / cellCount;
 			var cellSize = this.$refs.carousel.offsetWidth/cellCount;
-			var radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellRange ) );
-			var angle = theta * this.selectedIndex * -1;
-			this.$refs.carousel.style.transform = 'translateZ(' + -radius + 'px) ' +
-				'rotateY' + '(' + angle + 'deg)';
+			var radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellCount ) );
+			var angle = theta * this.selectedIndex /* -1*/;
+			this.$refs.carousel.style.transform = 'rotateY' + '(' + angle + 'deg) translateZ(' + radius + 'px) ';
 		},
 		next: function() {
 			this.selectedIndex++;
