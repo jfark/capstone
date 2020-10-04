86f9175e6c1ddf5208e8af2ccdb9916e24ec8caf
jfark <jessamyn.farkas@gmail.com>
2020-10-03 23:33:59 -0400

    Big Changes with .scene CSS
    Altering perspective from 1000 to 10000 on a whim seemed to have improved things a lot. Playing around with z-space "scenePerspective" as well.

--- script.js
+++ script.js
@@ -26,16 +26,14 @@ Vue.component('the-portfolio', {
 	},
 	methods: {
 		rotateCarousel: function() {
-			
-			
-			//var cellCount = 9;
-			//var cellRange = 9;
-			//var theta = 360 / cellCount;
-			//var cellSize = this.$refs.carousel.offsetWidth/cellCount;
-			//var radius = Math.round( (100000) / Math.tan(20) );
-			var angle = this.selectedIndex / 9 * -360;
-			this.$refs.carousel.style.transform = 
-				'rotateY(' + angle + 'deg)';
+			var cellCount = this.project.rooms.length;
+			var cellRange = 8;
+			var theta = 360 / cellRange;
+			var cellSize = this.$refs.carousel.offsetWidth/cellCount;
+			var radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellRange ) );
+			var angle = theta * this.selectedIndex * -1;
+			this.$refs.carousel.style.transform = 'translateZ(' + -radius + 'px) ' +
+							'rotateY' + '(' + angle + 'deg)';
 		},
 		next: function() {
 			this.selectedIndex++;
