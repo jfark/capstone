5e30cc3a9c739a8d1e4d05fb1698abf23f73365b
jfark <jessamyn.farkas@gmail.com>
2020-11-24 23:49:39 -0500

    Added artwork info to Carousel Titles

--- script.js
+++ script.js
@@ -74,47 +74,47 @@ var projectOne = new Vue ({
 					caption: 'Artwork info',
 				},
 				{
-					title: "...Title",
+					title: "Pierre-Auguste Renoir. Reclining Nude (La Source), c.1895.",
 					image: 'images/main-room-north-wall/BF903.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "Title",
+					title: "Paul Cézanne. Madame Cézanne with Green Hat (Madame Cézanne au chapeau vert), 1891–1892.",
 					image: 'images/main-room-north-wall/BF141.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "Title",
+					title: "Paul Cézanne. Gardener (Le Jardinier), c. 1885 (possibly later).",
 					image: './images/main-room-north-wall/BF534.jpg',
 					caption: 'Artwork info',
 				},
 				{
-					title: "Title",
+					title: "Paul Cézanne. The Bellevue Plain; The Red Earth (La Plaine de Bellevue; Les Terres rouges), 1890–1892.",
 					image: 'images/main-room-north-wall/BF909.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "Title",
+					title: "Pierre-Auguste Renoir. Girl with Basket of Fish (Pêcheuse de poissons), c. 1890.",
 					image: 'images/main-room-north-wall/BF53.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "Title",
+					title: "Paul Cézanne. Boy in a Red Vest (Le Garçon au gilet rouge), 1888–1890.",
 					image: './images/main-room-north-wall/BF20.jpg',
 					caption: 'Artwork info',
 				},
 				{
-					title: "Title; 1886–1888.",
+					title: "Pierre-Auguste Renoir. Bathing Group, 1916.",
 					image: 'images/main-room-north-wall/BF709.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "Title; 1886–1888.",
+					title: "Pierre-Auguste Renoir. Girl with Basket of Oranges (Marchande d'oranges), c. 1890.",
 					image: 'images/main-room-north-wall/BF65.jpg',
 					caption: 'Artwork info',
 
@@ -152,46 +152,46 @@ var projectTwo = new Vue ({
 					caption: 'Artwork info',
 				},
 				{
-					title: "Georges Seurat; Models (Poseuses); 1886–1888.",
+					title: "Georges Seurat. Models (Poseuses), 1886–1888.",
 					image: 'images/main-room-west-wall/BF811.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "Paul Cézanne; The Card Players (Les Joueurs de cartes); 1890–1892.",
+					title: "Paul Cézanne. The Card Players (Les Joueurs de cartes), 1890–1892.",
 					image: 'images/main-room-west-wall/BF564.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "Main Room, West Wall",
+					title: "Unidentified Maker. Door Hinge, 18th century.",
 					image: './images/main-room-west-wall/01.01.58.jpg',
 					caption: 'Artwork info',
 				},
 				{
-					title: "Georges Seurat; Models (Poseuses); 1886–1888.",
+					title: "Paul Cézanne. Leda and the Swan (Léda au cygne), c. 1880 (possibly later).",
 					image: 'images/main-room-west-wall/BF36.jpg',
 					caption: 'Artwork info',
 				},
 				{
-					title: "Paul Cézanne; The Card Players (Les Joueurs de cartes); 1890–1892.",
+					title: "Jean-Baptiste-Camille Corot. Mme de Larochenoire, Wife of the Painter (Mme de Larochenoire, femme du peintre), c. 1870.",
 					image: 'images/main-room-west-wall/BF533.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "Main Room, West Wall",
+					title: "Maurice Brazil Prendergast. The Beach 'No. 3', c. 1914–1915.",
 					image: './images/main-room-west-wall/BF359.jpg',
 					caption: 'Artwork info',
 				},
 				{
-					title: "Georges Seurat; Models (Poseuses); 1886–1888.",
+					title: "Henri Rousseau. View of the Quai d' Asnières (Vue du quai d'Asnières); also called The Canal and Landscape with Tree Trunks (Le Canal and Paysage avec troncs d'arbre), between April and December 1900.",
 					image: 'images/main-room-west-wall/BF583.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "Paul Cézanne; The Card Players (Les Joueurs de cartes); 1890–1892.",
+					title: "Unidentified Maker. Keyhole Escutcheon, 16th century.",
 					image: 'images/main-room-west-wall/01.01.63.jpg',
 					caption: 'Artwork info',
 
@@ -213,47 +213,47 @@ var projectThree = new Vue ({
 					caption: 'Artwork info',
 				},
 				{
-					title: "...Title",
+					title: "Paul Cézanne. The Large Bathers (Les Grandes baigneuses), 1895–1906.",
 					image: 'images/main-room-east-wall/BF934.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "...Title",
+					title: "William James Glackens. The Raft, 1915.",
 					image: 'images/main-room-east-wall/BF701.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "...Title",
+					title: "Tintoretto (Jacopo Robusti). Two Apostles, Late 16th century.",
 					image: './images/main-room-east-wall/BF807.jpg',
 					caption: 'Artwork info',
 				},
 				{
-					title: "...Title",
+					title: "Jean-Baptiste-Camille Corot. Mme Lemaistre, née Blanche Sennegon, Niece of Corot (Mme Lemaistre, née Blanche Sennegon, nièce de Corot), 1831.",
 					image: 'images/main-room-east-wall/BF895.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "...Title",
+					title: "Jean-Siméon Chardin. The Copper Water Urn, 1732–1740.",
 					image: 'images/main-room-east-wall/BF1198.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "...Title",
+					title: "Honoré Daumier. The Hypochondriac (Le Malade imaginaire), 1860–1863.",
 					image: './images/main-room-east-wall/BF75.jpg',
 					caption: 'Artwork info',
 				},
 				{
-					title: "...Title",
+					title: "Pierre-Auguste Renoir. Environs of Berneval, 1879.",
 					image: 'images/main-room-east-wall/BF6.jpg',
 					caption: 'Artwork info',
 
 				},
 				{
-					title: "...Title",
+					title: "Paul Cézanne. The Large Pear (La Grosse poire), 1895–1898.",
 					image: 'images/main-room-east-wall/BF190.jpg',
 					caption: 'Artwork info',
 
